ecf6ff4792988aa995ecd109719de4ad382af4a8
Mans-TTCore <mans.ttcore@gmail.com>
2019-09-09 01:45:02 -0400

    Initial Commit

---
+++ Macaw.GH/Macaw.GHInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace Macaw.GH
+{
+    public class MacawGHInfo : GH_AssemblyInfo
+  {
+    public override string Name
+    {
+        get
+        {
+            return "MacawGH";
+        }
+    }
+    public override Bitmap Icon
+    {
+        get
+        {
+            //Return a 24x24 pixel bitmap to represent this GHA library.
+            return null;
+        }
+    }
+    public override string Description
+    {
+        get
+        {
+            //Return a short string describing the purpose of this GHA library.
+            return "";
+        }
+    }
+    public override Guid Id
+    {
+        get
+        {
+            return new Guid("250c9dbf-d7c7-4280-844e-7bfa8bbe7c7d");
+        }
+    }
+
+    public override string AuthorName
+    {
+        get
+        {
+            //Return a string identifying you or your company.
+            return "";
+        }
+    }
+    public override string AuthorContact
+    {
+        get
+        {
+            //Return a string representing your preferred contact details.
+            return "";
+        }
+    }
+}
+}
---
+++ Macaw.GH/MacawViewer.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Grasshopper.GUI.Canvas;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Attributes;
+using Rhino.Geometry;
+
+// In order to load the result of this wizard, you will also need to
+// add the output bin/ folder of this project to the list of loaded
+// folder in Grasshopper.
+// You can use the _GrasshopperDeveloperSettings Rhino command for that.
+
+namespace Macaw.GH
+{
+    public class MacawViewer : GH_Component
+    {
+        public Image img = null;
+
+        /// <summary>
+        /// Each implementation of GH_Component must provide a public 
+        /// constructor without any arguments.
+        /// Category represents the Tab in which the component will appear, 
+        /// Subcategory the panel. If you use non-existing tab or panel names, 
+        /// new tabs/panels will automatically be created.
+        /// </summary>
+        public MacawViewer()
+          : base("Macaw", "Nickname",
+              "Description",
+              "Display", "A")
+        {
+        }
+        public override void CreateAttributes()
+        {
+            img = Properties.Resources.Macaw_sm;
+            m_attributes = new Attributes_Custom(this);
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Bitmap", "B", "A bitmap to preview", GH_ParamAccess.item);
+            pManager[0].Optional = true;
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
+        /// to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            Bitmap bmp = Properties.Resources.Macaw_sm;
+            if (!DA.GetData<Bitmap>(0, ref bmp));
+
+            img = bmp;
+        }
+
+        /// <summary>
+        /// Provides an Icon for every component that will be visible in the User Interface.
+        /// Icons need to be 24x24 pixels.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                // You can add image files to your project resources and access them like this:
+                //return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it. 
+        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("eba2de7a-71c8-485e-bae1-78660b645776"); }
+        }
+    }
+
+    public class Attributes_Custom : GH_ComponentAttributes
+    {
+        public Attributes_Custom(GH_Component owner) : base(owner) { }
+
+        private Rectangle ButtonBounds { get; set; }
+        protected override void Layout()
+        {
+            base.Layout();
+            MacawViewer comp = Owner as MacawViewer;
+
+            int width = comp.img.Width;
+            int height = comp.img.Height;
+            Rectangle rec0 = GH_Convert.ToRectangle(Bounds);
+
+            int cWidth = rec0.Width;
+            int cHeight = rec0.Height;
+
+            rec0.Width = width;
+            rec0.Height += height;
+
+            Rectangle rec1 = rec0;
+            rec1.Y = rec1.Bottom - height;
+            rec1.Height = height;
+            rec1.Width = width;
+
+            Bounds = rec0;
+            ButtonBounds = rec1;
+
+        }
+
+        protected override void Render(GH_Canvas canvas, Graphics graphics, GH_CanvasChannel channel)
+        {
+            base.Render(canvas, graphics, channel);
+            MacawViewer comp = Owner as MacawViewer;
+
+            if (channel == GH_CanvasChannel.Objects)
+            {
+                GH_Capsule capsule = GH_Capsule.CreateCapsule(ButtonBounds, GH_Palette.Normal,0, 0);
+                capsule.Render(graphics, Selected, Owner.Locked, true);
+                capsule.AddOutputGrip(this.OutputGrip.Y);
+                capsule.Dispose();
+                capsule = null;
+
+                StringFormat format = new StringFormat();
+                format.Alignment = StringAlignment.Center;
+                format.LineAlignment = StringAlignment.Center;
+
+                RectangleF textRectangle = ButtonBounds;
+                
+                graphics.DrawImage(comp.img,Bounds.X+2 , m_innerBounds.Y-(ButtonBounds.Height-Bounds.Height), comp.img.Width-4, comp.img.Height-2);
+
+                format.Dispose();
+            }
+        }
+    }
+}
