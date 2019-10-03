befe18b6410598f49fad359f21088625e17b3813
Mans-TTCore <36418123+Mans-TTCore@users.noreply.github.com>
2019-10-02 22:42:43 -0400

    WIP Filters

---
+++ Aviary.Macaw.GH/Filters/ApplyFilters.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Rhino.Geometry;
+
+namespace Aviary.Macaw.GH.Filters
+{
+    public class ApplyFilters : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the ApplyFilters class.
+        /// </summary>
+        public ApplyFilters()
+          : base("Apply Filters", "Apply", "Description", "Aviary 1", "Image")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Image", "I", "The Layer Bitmap", GH_ParamAccess.item);
+            pManager.AddBooleanParameter("Clear Filters", "C", "", GH_ParamAccess.item, false);
+            pManager[1].Optional = true;
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Image", "I", "The resulting image", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            Image image = new Image();
+            if (!DA.GetData(0, ref image)) return;
+            Image output = new Image(image);
+            Bitmap bitmap = output.GetFilteredBitmap();
+            image.Bitmap = bitmap;
+
+            DA.SetData(0, image);
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("b390afd0-9776-4a57-8c7a-0561e7955234"); }
+        }
+    }
+}
